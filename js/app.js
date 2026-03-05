ef0f1932e5df3b2a7a9d04236be01aedc6b225ee
ghassanalhammadeh <ghassanalhammadeh87@gmail.com>
2026-03-05 22:12:40 +0300

    modify products files

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function MyButton(){
+    return(
+        <button>
+            i'm a button
+        </button>
+    );
+}
+export default function MyApp(){
+    return(
+        <>
+        <h1>Welcome tomy app</h1>
+        <MyButton/>
+        </>
+    );
+}
